44c930354796f3162a79baaaf05fcf80b7407b3b
maksim <maksim.sokur90@gmail.com>
2021-12-06 10:32:13 +0300

    Add Matrix double!

--- matrixDiagonal/src/MatrixD.java
+++ matrixDiagonal/src/MatrixD.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 public class MatrixD {
 
     public static void main(String[] args) {
-        int[][] mat = {{-2, 31, 6, 7}, {15, -42, 1, 0}, {9, -7, 12, 19}, {}};
-
+        //int[][] mat = {{-2, 31, 6, 7}, {15, -42, 1, 0}, {9, -7, 12, 19}, {55, 34, 1, -10}};
+        int[][] mat = {{-2}};
 
         System.out.println(Arrays.toString(getDiagonal(mat)));
         System.out.println(Arrays.toString(getCounterDiagonal(mat)));
@@ -16,21 +16,23 @@ public class MatrixD {
         int[] result = new int[matrix.length];
         int count = 0;
         for (int i = 0; matrix.length > i; i++){
-            for (int j = 0; matrix.length  > j; j++){
-                if(i == 0 && j == 0) { result[count] = matrix[i][j]; }
-                if(i == 1 && j == 1) {count++; result[count] = matrix[i][j];}
-                if(i == 2 && j == 2) {count++; result[count] = matrix[i][j];}
+            for (int j = 0; matrix.length > j; j++){
+                if (i == count && j == count) { result[count] = matrix[i][j]; count++;}
             }
         }
             return result;
     }
 
     public static int[] getCounterDiagonal(int[][] matrix) {
-        int[] result = new int[matrix[0].length];
+        int[] result = new int[matrix.length];
         int count = 0;
+        int find = matrix.length - 1;
         for (int i = 0; matrix.length > i; i++){
-            for (int j = 0; matrix.length > j; j++){
-                if (i == 0 && j == (matrix[i].length - 1) /2) { result[count] = matrix[i][j];}
+            for (int j = matrix[i].length - 1 ; j >= 0; --j){
+                if (find >= 0 ) {
+                    result[count] = matrix[count][find];
+                count++;
+                find--;}
             }
         }
         return result;
